0e08aa01d288da5aa157a3ee92dd650b9240bdb6
executeFahry <fahrybaihaki0105@gmail.com>
2024-09-26 22:10:48 +0700

    Display all data values in homepage with box info

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,27 @@
 
 namespace App\Controllers;
 
+use App\Models\JenisUsahaModel;
+use App\Models\UmkmModel;
+use App\Models\BobotModel;
+use App\Models\KriteriaModel;
+
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
-        return view('home');
+        $jenisUsahaModel = new JenisUsahaModel();
+        $umkmModel = new UmkmModel();
+        $bobotModel = new BobotModel();
+        $kriteriaModel = new KriteriaModel();
+
+        $data = [
+            'jenisUsahaCount' => $jenisUsahaModel->countAllResults(),
+            'umkmCount' => $umkmModel->countAllResults(),
+            'bobotCount' => $bobotModel->countAllResults(),
+            'kriteriaCount' => $kriteriaModel->countAllResults(),
+        ];
+
+        return view('home', $data);
     }
 }
--- app/Views/main/v_content.php
+++ app/Views/main/v_content.php
@@ -26,7 +26,9 @@
                     <!-- small box -->
                     <div class="small-box bg-info">
                         <div class="inner">
-                            <h3>150</h3>
+                            <h3>
+                                <?= $jenisUsahaCount ?>
+                            </h3>
 
                             <p>Data Jenis Usaha</p>
                         </div>
@@ -41,7 +43,9 @@
                     <!-- small box -->
                     <div class="small-box bg-success">
                         <div class="inner">
-                            <h3>53<sup style="font-size: 20px">%</sup></h3>
+                            <h3>
+                                <?= $umkmCount ?>
+                            </h3>
 
                             <p>Data UMKM</p>
                         </div>
@@ -56,7 +60,9 @@
                     <!-- small box -->
                     <div class="small-box bg-warning">
                         <div class="inner">
-                            <h3>44</h3>
+                            <h3>
+                                <?= $bobotCount ?>
+                            </h3>
 
                             <p>Data Bobot</p>
                         </div>
@@ -71,7 +77,9 @@
                     <!-- small box -->
                     <div class="small-box bg-danger">
                         <div class="inner">
-                            <h3>65</h3>
+                            <h3>
+                                <?= $kriteriaCount ?>
+                            </h3>
 
                             <p>Data Kriteria</p>
                         </div>
